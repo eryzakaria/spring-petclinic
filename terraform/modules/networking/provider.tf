976aaae128556719e8da68ea20561fa7b74d4d37
eryzakaria <eryzakariatrimadhani14@gmail.com>
2024-10-24 21:57:49 +0700

    update

--- terraform/modules/networking/provider.tf
+++ terraform/modules/networking/provider.tf
@@ -1,5 +1,5 @@
 provider "google" {
-  credentials = file("<path-to-your-service-account-key>.json")
+  credentials = file("${path.module}/secrets/mythic-fire-439515-v0-e11e9d7da93c.json")
   project     = var.project_id
   region      = var.region
 }
